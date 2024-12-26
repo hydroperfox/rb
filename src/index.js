6c42950ad9fcc100c00f4ba1605ec3be7bf24710
hydroperfox <hydroperfox@gmail.com>
2024-12-26 09:36:24 -0300

    Update

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import { program } from "commander";
+
+program
+    .name("hydroperrb")
+    .description("Reference portal builder.")
+    .version("1.0.0");
+
+program
+    .command("build")
+    .description("Build the reference portal.")
+    .action(options => {
+        // do it
+    });
